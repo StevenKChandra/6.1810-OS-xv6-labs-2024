ae0d368bcf1e08b2bc66d4bf9e3885d874a030c7
Steven Kurniawan Chandra <94112051+StevenKChandra@users.noreply.github.com>
2025-02-17 01:59:22 +0000

    added:     implementation of sleep util command (user/sleep.c) modified:     git ignore to ignore .vscode     ./MakeFile to compile sleep.c

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int t = atoi(argv[1]);
+    if (argc == 1) {
+        char* error_message = "No argument passed for sleep\n";
+        write(1, error_message, strlen(error_message));
+    }
+    else if (argc == 2) {
+        sleep(t);
+    }
+    else {
+        char* error_message = "Too many argument passed for sleep\n";
+        write(1, error_message, strlen(error_message));
+    }
+    exit(0);
+}
